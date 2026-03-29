5a0ea7970187afd8d18d3db4972093dbcfe168a4
tahaaboubakr-DLIGHT <taha.aboubakr@gmail.com>
2026-03-29 05:52:01 +0100

    Update supabase.js

--- dlight-achats/lib/supabase.js
+++ dlight-achats/lib/supabase.js
@@ -4,10 +4,9 @@ let supabase = null;
 
 export function getSupabase() {
   if (supabase) return supabase;
-  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-  if (url && key) {
-    supabase = createClient(url, key);
-  }
+  supabase = createClient(
+    "https://sflruygvehgijvphdtvm.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNmbHJ1eWd2ZWhnaWp2cGhkdHZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MzU4MTEsImV4cCI6MjA5MDIxMTgxMX0.5ZI8OKHkgqTcgrD5ew23kBhDa8jFwFpBi5i2jufJl70"
+  );
   return supabase;
 }
